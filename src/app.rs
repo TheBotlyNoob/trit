25e0f6069235dcec784a2b2e4d0d62c0e3a0117d
Jay D Jackson <jj@jj.is-a.dev>
2023-04-14 19:41:20 +0000

    put all platform-specific code into main.rs

--- src/app.rs
+++ src/app.rs
@@ -1,105 +1,47 @@
 use softbuffer::GraphicsContext;
-use tiny_skia::{
-    FillRule, FilterQuality, Paint, PathBuilder, Pattern, Pixmap, SpreadMode, Transform,
-};
+use tiny_skia::{ClipMask, FillRule, Paint, PathBuilder, Pixmap, Rect, Transform};
+use winit::dpi::PhysicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
+use winit::window::Window;
 
-pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop)?;
-
-    #[cfg(target_arch = "wasm32")]
-    {
-        use winit::platform::web::WindowExtWebSys;
-
-        window.set_inner_size(winit::dpi::PhysicalSize {
-            width: web_sys::window()
-                .unwrap()
-                .inner_width()
-                .unwrap()
-                .as_f64()
-                .unwrap(),
-            height: web_sys::window()
-                .unwrap()
-                .inner_height()
-                .unwrap()
-                .as_f64()
-                .unwrap(),
-        });
-
-        web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .body()
-            .unwrap()
-            .append_child(&window.canvas())
-            .unwrap();
-    }
-
-    let mut graphics_context = unsafe { GraphicsContext::new(&window, &window) }?;
-
+pub async fn event_loop(
+    window: Window,
+    event_loop: EventLoop<()>,
+    mut gfx_ctx: GraphicsContext,
+) -> Result<(), Box<dyn std::error::Error>> {
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
             Event::RedrawRequested(window_id) if window_id == window.id() => {
-                let (width, height) = {
-                    let size = window.inner_size();
-                    (size.width, size.height)
-                };
-
-                let triangle = crate_triangle();
-
-                let paint = Paint {
-                    anti_alias: true,
-                    shader: Pattern::new(
-                        triangle.as_ref(),
-                        SpreadMode::Repeat,
-                        FilterQuality::Bicubic,
-                        1.0,
-                        Transform::from_row(1.5, -0.4, 0.0, -0.8, 5.0, 1.0),
-                    ),
-                    ..Default::default()
-                };
-
-                let path = PathBuilder::from_circle(200.0, 200.0, 180.0).unwrap();
-
+                let PhysicalSize { width, height } = window.inner_size();
                 let mut pixmap = Pixmap::new(width, height).unwrap();
-                pixmap.fill_path(
-                    &path,
+
+                let clip_path = {
+                    let mut pb = PathBuilder::new();
+                    pb.push_circle(250.0, 250.0, 200.0);
+                    pb.push_circle(250.0, 250.0, 100.0);
+                    pb.finish().unwrap()
+                };
+
+                let clip_path = clip_path
+                    .transform(Transform::from_row(1.0, -0.3, 0.0, 1.0, 0.0, 75.0))
+                    .unwrap();
+
+                let mut clip_mask = ClipMask::new();
+                clip_mask.set_path(500, 500, &clip_path, FillRule::EvenOdd, true);
+
+                let mut paint = Paint::default();
+                paint.set_color_rgba8(50, 127, 150, 200);
+                pixmap.fill_rect(
+                    Rect::from_xywh(0.0, 0.0, 500.0, 500.0).unwrap(),
                     &paint,
-                    FillRule::Winding,
                     Transform::identity(),
-                    None,
+                    Some(&clip_mask),
                 );
 
-                fn crate_triangle() -> Pixmap {
-                    let mut paint = Paint::default();
-                    paint.set_color_rgba8(50, 127, 150, 200);
-                    paint.anti_alias = true;
-
-                    let mut pb = PathBuilder::new();
-                    pb.move_to(0.0, 20.0);
-                    pb.line_to(20.0, 20.0);
-                    pb.line_to(10.0, 0.0);
-                    pb.close();
-                    let path = pb.finish().unwrap();
-
-                    let mut pixmap = Pixmap::new(20, 20).unwrap();
-                    pixmap.fill_path(
-                        &path,
-                        &paint,
-                        FillRule::Winding,
-                        Transform::identity(),
-                        None,
-                    );
-                    pixmap
-                }
-
-                graphics_context.set_buffer(
+                gfx_ctx.set_buffer(
                     bytemuck::cast_slice(pixmap.pixels()),
                     width as u16,
                     height as u16,
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,62 @@
+use softbuffer::GraphicsContext;
+use winit::{event_loop::EventLoop, window::WindowBuilder};
+
 mod app;
 
-async fn main_() {
-    if let Err(e) = app::main().await {
-        tracing::error!("Error: {}", e);
+// bootstrap for both native and wasm - the main code is in `app.rs`
+
+async fn main_() -> Result<(), Box<dyn std::error::Error>> {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop)?;
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use winit::platform::web::WindowExtWebSys;
+
+        window.set_inner_size(winit::dpi::PhysicalSize {
+            width: web_sys::window()
+                .unwrap()
+                .inner_width()
+                .unwrap()
+                .as_f64()
+                .unwrap(),
+            height: web_sys::window()
+                .unwrap()
+                .inner_height()
+                .unwrap()
+                .as_f64()
+                .unwrap(),
+        });
+
+        web_sys::window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .body()
+            .unwrap()
+            .append_child(&window.canvas())
+            .unwrap();
     }
+
+    // SAFETY: both the window and the graphics context live for the life of main
+    let gfx_ctx = unsafe { GraphicsContext::new(&window, &window) }?;
+
+    app::event_loop(window, event_loop, gfx_ctx).await
 }
 
 fn main() {
+    let fut = async {
+        if let Err(e) = main_().await {
+            tracing::error!("Error: {}", e);
+        }
+    };
+
     #[cfg(target_arch = "wasm32")]
     {
         console_error_panic_hook::set_once();
         tracing_wasm::set_as_global_default();
 
-        wasm_bindgen_futures::spawn_local(main_());
+        wasm_bindgen_futures::spawn_local(fut);
     }
 
     #[cfg(not(target_arch = "wasm32"))]
@@ -23,7 +67,7 @@ fn main() {
             .enable_all()
             .build()
             .unwrap();
-        rt.block_on(main_());
+        rt.block_on(fut);
     }
 }
 #[cfg(target_arch = "wasm32")]
